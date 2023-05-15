29666b8ccdb7fd511fd945e8e3a611488800c8ad
Jayakrishnan-manoj <91311543+Jayakrishnan-manoj@users.noreply.github.com>
2023-05-15 22:38:23 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/responsive/mobilescreen_layout.dart';
+import 'package:instagram_clone/responsive/responsive_screen.dart';
+import 'package:instagram_clone/responsive/webscreen_layout.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Instagram-clone',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      home: const ResponsiveLayout(
+        webScreenLayout: WebScreenLayout(),
+        mobileScreenLayout: MobileScreenLayout(),
+      ),
+    );
+  }
+}
